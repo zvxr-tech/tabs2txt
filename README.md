530e82e12f751f798ccbb13599a7a171a2700370
Mike Clark <zvxr-tech@users.noreply.github.com>
2017-03-20 10:33:33 -0600

    release 1.1 - Select all text when popup appears so that user can use copy clipboard. Shortcut without clicking button. Only copy urls from tabs in active window. Added capability to open tabs from list in textArea

--- README.md
+++ README.md
@@ -4,10 +4,20 @@
 Mike Clark 2016 
 
 ## Description
-Concatenate all of the URLs of all the tabs in a window and give the option to 
-copy to clipboard.
+This add-on provides a button to copy the URL strings of all active tabs in a window to a scratchpad, delimit them with newlines, and provides the ability to copy the list to clipboard.
 
 ## Compatability
 Tested on:
 - Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0
 - Chrome Version 56.0.2924.87 (64-bit)
+
+## Changelog
+Version 1.1
+- select all text when popup appears so that user can use copy clipboard 
+ shortcut without clicking button
+- only copy urls from tabs in active window
+- added capability to open tabs from list in textArea
+
+Version 1.0
+- Basic functionality: copy all tabs in window to textarea
+
--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,3 @@
-/* This code file is executed each time the popup loads */
-
-
 /* Select the text and copy to clipboard, when that button is clicked */
 document.querySelector('#copy-button').onclick = function(e) {		
 		let textArea = document.querySelector('#popup-textarea');
@@ -9,11 +6,34 @@ document.querySelector('#copy-button').onclick = function(e) {
 		//console.log('copied');
 }
 
-/* Iterate over the tabs
- * works in Chrome && Firefox, others?
- */
-var queryInfo = {};
-chrome.tabs.query(queryInfo, 
+
+function onCreated(tab) {
+  //console.log(`Created new tab: ${tab.id} with url ${tab.url} `)
+}
+
+function onError(error) {
+  console.log(`tabs2txt error:open_tabs  ${error}`);
+}
+
+/* Open each newline delimited URI in the textarea as a new tab */
+document.querySelector('#paste-button').onclick = function(e) {
+	let urlArray = document.querySelector('#popup-textarea').value.split("\n");
+	
+	for (let url of urlArray) {
+		let trimURL = url.trim();
+		if (trimURL) {
+			var creating = browser.tabs.create({
+				url:trimURL,
+				active:false
+			});
+			creating.then(null, onError);
+		}
+	}
+	//console.log(urlArray);
+}
+
+/* Iterate over the tabs */
+chrome.tabs.query({currentWindow:true},
 	function queryCallback(tabs) {
 		let txt = "";
 		for (let tab of tabs) {
@@ -23,9 +43,8 @@ chrome.tabs.query(queryInfo,
 		txt = txt.slice(0, -1); // chop off the last record separator
 		let textArea = document.querySelector('#popup-textarea');
 		textArea.value = txt;
-		textArea.select();
+		textArea.selectionStart=0;
+		textArea.selectionEnd=textArea.textLength;
+		textArea.focus();
 		//console.log(txt);
 });
-
-/* Set focus on the copy button */
-document.querySelector('#copy-button').focus();
